a8b80fec292978aeea42c21558e54e5b091f2950
alecasentini <ale.casentini@gmail.com>
2023-03-28 11:01:24 +0200

    Snack1

---
+++ snack1/assets/js/main.js
@@ -0,0 +1,13 @@
+let a = prompt ("inserisci un numero")
+
+let b = prompt ("inserisci un numero")
+
+if (a > b ){
+    console.log (`il numero maggiore è ${a}`)
+}
+else if( a < b ){
+    console.log (`il numero maggiore è ${b}`)
+}
+else{
+    console.log ("i numeri sono uguali")
+}
